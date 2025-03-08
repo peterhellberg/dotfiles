47a2999a4641fb57bf85b4690b2c9c4047fc81e9
Peter Hellberg <peter@c7.se>
2025-03-08 22:32:44 +0100

    linux/nvim: Update init.lua

--- linux/nvim/init.lua
+++ linux/nvim/init.lua
@@ -17,7 +17,6 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
     ['<Tab>'] = cmp.mapping(function(fallback) 
