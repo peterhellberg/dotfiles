9a7e998081abc18827bea87f1feaf701846e43ce
Peter Hellberg <peter@c7.se>
2025-04-10 15:49:26 +0200

    macos/nvim: Add configs, including light.lua

---
+++ macos/nvim/light.lua
@@ -0,0 +1,8 @@
+local base = vim.fn.stdpath("config") .. "/base.lua"
+vim.cmd.source(base)
+
+local vimrc = vim.fn.stdpath("config") .. "/init.vim"
+vim.cmd.source(vimrc)
+
+vim.o.background = "light"
+vim.cmd.colorscheme "lunaperche"
