c8136af9c2a748f453120fd3bc05d44e1eede3da
Peter Hellberg <peter@c7.se>
2025-10-08 12:13:49 +0200

    macos/nvim/after/lsp/gopls: on_attach

--- macos/nvim/after/lsp/gopls.lua
+++ macos/nvim/after/lsp/gopls.lua
@@ -4,6 +4,29 @@ return {
       settings = client.config.settings,
     })
   end,
+  on_attach = function(client, bufnr)
+    local orig_notify = vim.notify
+
+    vim.notify = function(msg, ...)
+      if msg:match("position_encoding param is required") then return end
+      orig_notify(msg, ...)
+    end
+
+    local params = vim.lsp.util.make_range_params()
+    params.context = {only = {"source.organizeImports"}}
+
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+
+    vim.notify = orig_notify
+  end,
   settings = {
     gopls = {
       analyses = {
