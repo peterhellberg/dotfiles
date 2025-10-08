8431ac8b168c2ec93cd10b78de8b055a7d5ffe46
Peter Hellberg <peter@c7.se>
2025-10-08 12:12:57 +0200

    macos/nvim: Format and organize imports for Go

--- macos/nvim/lua/config/autocmds.lua
+++ macos/nvim/lua/config/autocmds.lua
@@ -23,13 +23,25 @@ vim.api.nvim_create_autocmd('BufWritePre',{
   end
 })
 
--- Organize imports for Go
+-- Format and organize imports for Go
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
-  callback = function()
+  callback = function(args)
+    local bufnr = args.buf
+
+    pcall(vim.lsp.buf.format, {
+      bufnr = bufnr,
+      async = false,
+    })
+
     local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
+
+    params.context = {
+      only = { "source.organizeImports" }
+    }
+
     local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 1000)
+
     for _, res in pairs(result or {}) do
       for _, action in pairs(res.result or {}) do
         if action.edit then
