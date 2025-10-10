fe04b40fa6952ac6e952d70ef2c099d5f852f0c4
Peter Hellberg <peter@c7.se>
2025-10-10 19:00:53 +0200

    macos/nvim/lua/config/commands: Add Path

--- macos/nvim/lua/config/commands.lua
+++ macos/nvim/lua/config/commands.lua
@@ -1,3 +1,9 @@
+-- Pt
 vim.api.nvim_create_user_command('Pt', function(opts)
   vim.cmd('Ack ' .. table.concat(opts.fargs, ' '))
+end, { nargs = '*' })
+
+-- Path
+vim.api.nvim_create_user_command('File', function(opts)
+  print(vim.fn.expand('%:p'))
 end, { nargs = '*' })
