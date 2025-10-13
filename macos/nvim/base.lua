20dcbf803208c21468202790e9f8562c46dfc08b
Peter Hellberg <peter@c7.se>
2025-10-13 21:49:30 +0200

    macos/nvim/base: No dofile of local.lua/after.lua

--- macos/nvim/base.lua
+++ macos/nvim/base.lua
@@ -1,9 +1,3 @@
-local local_config = vim.fn.expand("~/.config/nvim/local.lua")
-
-if vim.fn.filereadable(local_config) == 1 then
-    dofile(local_config)
-end
-
 require("core.cmp")
 require("core.lsp")
 require("config.options")
@@ -11,9 +5,3 @@ require("config.keymaps")
 require("config.autocmds")
 require("config.commands")
 require("config.alternates")
-
-local after_config = vim.fn.expand("~/.config/nvim/after.lua")
-
-if vim.fn.filereadable(after_config) == 1 then
-    dofile(after_config)
-end
