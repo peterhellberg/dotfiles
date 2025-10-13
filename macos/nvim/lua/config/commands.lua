e5087f8a1eedaeea2d127d64c6c9106b0dc80799
Peter Hellberg <peter@c7.se>
2025-10-13 13:13:55 +0200

    macos/nvim/lua/config/commands: Add Diary and Wiki commands

--- macos/nvim/lua/config/commands.lua
+++ macos/nvim/lua/config/commands.lua
@@ -7,3 +7,18 @@ end, { nargs = '*' })
 vim.api.nvim_create_user_command('File', function(opts)
   print(vim.fn.expand('%:p'))
 end, { nargs = '*' })
+
+-- Diary
+vim.api.nvim_create_user_command('DiaryIndex', function()
+  vim.cmd('silent VimwikiDiaryIndex')
+  vim.cmd('VimwikiDiaryGenerateLinks')
+end, { nargs = '*' })
+
+vim.api.nvim_create_user_command('DiaryToday', function(opts)
+  vim.cmd('VimwikiMakeDiaryNote' .. table.concat(opts.fargs, ' '))
+end, { nargs = '*' })
+
+-- Wiki
+vim.api.nvim_create_user_command('Wiki', function(opts)
+  vim.cmd('VimwikiGoto ' .. table.concat(opts.fargs, ' '))
+end, { nargs = '*' })
