695b8becdc010ffc203cc5890b6d65de0966b1e4
Peter Hellberg <peter@c7.se>
2026-04-01 10:13:24 +0200

    macos/nvim: Use vim.pack

--- macos/nvim/init.lua
+++ macos/nvim/init.lua
@@ -1,3 +1,24 @@
+vim.pack.add {
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/L3MON4D3/LuaSnip',
+  'https://github.com/hrsh7th/nvim-cmp',
+  'https://github.com/saadparwaiz1/cmp_luasnip',
+  'https://github.com/hrsh7th/cmp-nvim-lsp',
+  'https://github.com/hrsh7th/vim-vsnip',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/vimwiki/vimwiki',
+  'https://github.com/preservim/nerdtree',
+  'https://github.com/itchyny/lightline.vim',
+  'https://github.com/airblade/vim-gitgutter',
+  'https://github.com/tpope/vim-fugitive',
+  'https://github.com/Yggdroot/LeaderF',
+  'https://github.com/honza/vim-snippets',
+  'https://github.com/mileszs/ack.vim',
+}
+
+require('oil').setup()
+
 require("functions")
 require("commands")
 require("core")
