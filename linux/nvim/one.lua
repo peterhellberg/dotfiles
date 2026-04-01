f0f34d52d58d6c30b832599d09c73ef4c4e4679f
Peter Hellberg <peter@c7.se>
2026-04-01 10:13:42 +0200

    linux/nvim: Use vim.pack

--- linux/nvim/one.lua
+++ linux/nvim/one.lua
@@ -1,3 +1,43 @@
+vim.pack.add {
+  'https://github.com/L3MON4D3/LuaSnip',
+  'https://github.com/hrsh7th/nvim-cmp',
+  'https://github.com/saadparwaiz1/cmp_luasnip',
+  'https://github.com/hrsh7th/cmp-nvim-lsp',
+  'https://github.com/hrsh7th/vim-vsnip',
+  'https://github.com/neovim/nvim-lspconfig',
+  'https://github.com/stevearc/oil.nvim',
+  'https://github.com/vimwiki/vimwiki',
+  'https://github.com/airblade/vim-gitgutter',
+  'https://github.com/tpope/vim-fugitive',
+  'https://github.com/Yggdroot/LeaderF',
+  'https://github.com/honza/vim-snippets',
+  'https://github.com/mileszs/ack.vim',
+}
+
+local oil = require("oil")
+
+oil.setup({
+  use_default_keymaps = false,
+  keymaps = {
+    ["h"] = "actions.parent",
+    ["l"] = "actions.select",
+    ["<CR>"] = "actions.select",
+    ["s"] = { "actions.select", opts = { vertical = true } },
+    ["t"] = { "actions.select", opts = { tab = true } },
+    ["q"] = "actions.close",
+    ["g?"] = "actions.show_help",
+    ["g."] = "actions.toggle_hidden",
+  },
+  float = {
+    border = "rounded",
+    max_width = 0.4,
+    max_height = 0.8,
+    win_options = {
+      winblend = 0
+    },
+  },
+})
+
 vim.o.background = "dark"
 vim.cmd([[colorscheme default]])
 
@@ -19,7 +59,9 @@ vim.g.mapleader = ","
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-keymap("n", "<Leader>n", "<cmd>Lexplore %:p:h<CR>")
+keymap("n", "<Leader>n", function()
+  oil.open_float(vim.fn.expand("%:p:h"))
+end, opts)
 keymap('n', '<Leader>s<Left>', ':leftabove vnew<CR>', opts)
 keymap('n', '<Leader>s<Right>', ':rightbelow vnew<CR>', opts)
 keymap('n', '<Leader>s<Up>', ':leftabove new<CR>', opts)
@@ -43,7 +85,7 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "*",
   callback = function()
     if vim.bo.filetype ~= "zig" then
-      vim.api.nvim_buf_set_option(0, "omnifunc", "")  -- disable default omnifunc
+      vim.bo.omnifunc = "" -- disable default omnifunc
     end
   end,
 })
@@ -51,7 +93,7 @@ vim.api.nvim_create_autocmd("FileType", {
 local zls_cmd = { "zls" }
 local zls_settings = {}
 
-local function on_attach(client, bufnr)
+local function on_attach(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", ":lua vim.lsp.buf.definition()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":lua vim.lsp.buf.references()<CR>", opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "§", ":lua vim.lsp.buf.hover({border = 'rounded', max_width = 80})<CR>", opts)
@@ -71,7 +113,7 @@ local function start_zls()
     on_attach = on_attach,
     filetypes = { "zig" },
   })
-  vim.api.nvim_buf_set_option(0, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
 
   -- Format-on-save for this buffer only
   vim.api.nvim_create_autocmd("BufWritePre", {
@@ -150,6 +192,9 @@ hi(0, "Directory", { fg = "#FFB964" })
 hi(0, "ErrorMsg", { fg = "#605958" })
 hi(0, "FloatBorder", { fg = "#555555" })
 hi(0, "Function", { fg = "#FAD07A" })
+hi(0, "GitGutterAdd",    { fg = "#97a950", bg = "#2D3218" })
+hi(0, "GitGutterChange", { fg = "#FFB964", bg = "#4c371e" })
+hi(0, "GitGutterDelete", { fg = "#d35738", bg = "#3f1a10" })
 hi(0, "Identifier", { fg = "#C6B6EE" })
 hi(0, "Include", { fg = "#8FBFDC" })
 hi(0, "Keyword", { fg = "#8197BE" })
@@ -168,8 +213,9 @@ hi(0, "PreProc", { fg = "#8FBFDC" })
 hi(0, "Question", { fg = "#E6E6CD" })
 hi(0, "Repeat", { fg = "#8197BF" })
 hi(0, "Search", { fg = "#404040", bg = "#EFEFEF" })
-hi(0, "SpecialChar", { fg = "#C6B6EE" })
+hi(0, "SignColumn", { link = "LineNr" })
 hi(0, "Special", { fg = "#799D6A" })
+hi(0, "SpecialChar", { fg = "#C6B6EE" })
 hi(0, "Statement", { fg = "#8197BE" })
 hi(0, "StatusLine", { fg = "#9E9E9E", bg = "none" })
 hi(0, "StorageClass", { fg = "#C59F6F" })
@@ -178,3 +224,60 @@ hi(0, "Structure", { fg = "#8FBFDC" })
 hi(0, "Type", { fg = "#FFB964" })
 hi(0, "Visual", { bg = "#303030" })
 hi(0, "WinSeparator", { fg = "#333333" })
+
+-- Markdown highlights
+hi(0, "markdownH1", { fg = "#fad17a" })
+hi(0, "markdownH2", { fg = "#ffb372" })
+hi(0, "markdownH3", { fg = "#ffb372" })
+hi(0, "markdownH4", { fg = "#ffb372" })
+hi(0, "markdownH5", { fg = "#ffb372" })
+hi(0, "markdownH1Delimiter", { fg = "#fad17a" })
+hi(0, "markdownH2Delimiter", { fg = "#ffb372" })
+hi(0, "markdownH3Delimiter", { fg = "#ffb372" })
+hi(0, "markdownH4Delimiter", { fg = "#ffb372" })
+hi(0, "markdownH5Delimiter", { fg = "#ffb372" })
+hi(0, "markdownCode", { fg = "#8fbfdc" })
+hi(0, "markdownCodeBlock", { fg = "#8fbfdc" })
+hi(0, "markdownCodeDelimiter", { fg = "#4d636f" })
+hi(0, "markdownAutomaticLink", { fg = "#cf6a4c" })
+hi(0, "markdownUrlDelimiter", { fg = "#874937" })
+
+-- Link Vimwiki headers to Markdown highlights
+hi(0, "VimwikiHeader1", { link = "markdownH1" })
+hi(0, "VimwikiHeader2", { link = "markdownH2" })
+hi(0, "VimwikiHeader3", { link = "markdownH3" })
+hi(0, "VimwikiHeader4", { link = "markdownH4" })
+hi(0, "VimwikiHeader5", { link = "markdownH5" })
+hi(0, "VimwikiHeaderChar", { fg = "#fad17a" })
+hi(0, "VimwikiListTodo", { fg = "#8197be" })
+
+-- Vimwiki
+vim.g.vimwiki_use_calendar = 1
+vim.g.vimwiki_url_maxsave = 0
+vim.g.vimwiki_global_ext = 0
+
+vim.g.vimwiki_list = {
+  {
+    path = '~/Documents/Wiki/',
+    syntax = 'markdown',
+    ext = '.md',
+    auto_tags = 1,
+    auto_diary_index = 1,
+    automatic_nested_syntaxes = 1,
+    nested_syntaxes = {
+      csv = 'csv',
+      go = 'go',
+      json = 'json',
+      make = 'make',
+      sh = 'sh',
+      sql = 'sql',
+      yaml = 'yaml',
+      xml = 'xml',
+      zig = 'zig',
+    },
+  }
+}
+
+-- Key mappings
+vim.api.nvim_set_keymap('n', '<Leader>vn', '<Plug>VimwikiNextLink', { noremap = false, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>vp', '<Plug>VimwikiPrevLink', { noremap = false, silent = true })
