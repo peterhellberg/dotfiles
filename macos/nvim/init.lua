f2156b06cbd8e93054d8ae035c02ef67de813427
Peter Hellberg <peter@c7.se>
2026-04-07 21:45:26 +0200

    macos/nvim: Add nvim-treesitter again

--- macos/nvim/init.lua
+++ macos/nvim/init.lua
@@ -1,4 +1,5 @@
 vim.pack.add {
+  'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/L3MON4D3/LuaSnip',
   'https://github.com/hrsh7th/nvim-cmp',
   'https://github.com/saadparwaiz1/cmp_luasnip',
