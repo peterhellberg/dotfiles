e6f0e9b39ea0d1ad76468baf4e8769ffc6535a3f
Peter Hellberg <peter@c7.se>
2026-04-11 08:46:01 +0200

    macos/nvim: Toggle Oil, remove NERDTree and Ack plugins

--- macos/nvim/init.lua
+++ macos/nvim/init.lua
@@ -8,13 +8,11 @@ vim.pack.add {
   'https://github.com/neovim/nvim-lspconfig',
   'https://github.com/stevearc/oil.nvim',
   'https://github.com/vimwiki/vimwiki',
-  'https://github.com/preservim/nerdtree',
   'https://github.com/nvim-lualine/lualine.nvim',
   'https://github.com/airblade/vim-gitgutter',
   'https://github.com/tpope/vim-fugitive',
   'https://github.com/Yggdroot/LeaderF',
   'https://github.com/honza/vim-snippets',
-  'https://github.com/mileszs/ack.vim',
   'https://github.com/kungfusheep/mfd.nvim',
 }
 
@@ -26,10 +24,23 @@ require("lsp")
 local vimrc = vim.fn.stdpath("config") .. "/vimrc.vim"
 vim.cmd.source(vimrc)
 
+require('vim._core.ui2').enable({})
+
+vim.o.smoothscroll = true
+vim.o.pumblend = 10
+vim.o.winblend = 10
+vim.o.laststatus = 3
+vim.o.shortmess = vim.o.shortmess .. "C"
+vim.o.winborder = "rounded"
+
 local oil = require("oil")
 
 oil.setup({
-  use_default_keymaps = false,
+ use_default_keymaps = false,
+  default_file_explorer = true,
+  view_options = {
+    show_hidden = true,
+  },
   keymaps = {
     ["h"] = "actions.parent",
     ["l"] = "actions.select",
@@ -41,21 +52,39 @@ oil.setup({
   },
   float = {
     border = "rounded",
-    max_width = 0.4,
-    max_height = 0.8,
+    max_width = 0.5,
+    max_height = 0.6,
     win_options = {
       winblend = 0
     },
+    override = function(conf)
+      conf.row = 1
+      conf.col = math.floor((vim.o.columns - conf.width) / 2)
+      return conf
+    end,
   },
 })
 
-require('vim._core.ui2').enable({})
+local function is_oil_open()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local buf = vim.api.nvim_win_get_buf(win)
+    local name = vim.api.nvim_buf_get_name(buf)
+    if name:sub(1, 6) == "oil://" then
+      return win
+    end
+  end
+end
 
-vim.o.smoothscroll = true
-vim.o.pumblend = 10
-vim.o.winblend = 10
-vim.o.laststatus = 3
-vim.o.shortmess = vim.o.shortmess .. "C"
+local function toggle_oil()
+  local win = is_oil_open()
+  if win then
+    vim.api.nvim_win_close(win, true)
+  else
+    oil.open_float()
+  end
+end
+
+vim.keymap.set("n", "<leader>n", toggle_oil, { desc = "Toggle Oil" })
 
 local sections = {
   lualine_a = {
